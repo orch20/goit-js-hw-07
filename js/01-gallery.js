f85e446339b3ae4e5c780a7862cc11ba5f184bb2
orch20 <94862153+orch20@users.noreply.github.com>
2022-05-19 22:27:41 +0300

    added showGallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,18 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector("gallery"),
+};
+
+const showGallery = () => {
+  return galleryItems
+    .map(
+      (el) => `<img src="${el.preview}" alt="${el.description}" data-src="#" />`
+    )
+    .join("");
+};
+console.log(showGallery());
+
+refs.gallery.innerHTML = showGallery();
